c803122f15175868216fad936a0058d5be3f2a38
esotericabsurdist <esotericabsurdist@gmail.com>
2016-10-31 15:13:18 -0700

    Got GET for /question working

--- server.js
+++ server.js
@@ -4,26 +4,46 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var mongoose = require('mongoose');
 var PORT = process.env.PORT || 3000;
-
-
-
+require('./db.js'); // MongoDB COnnection.
+var db = mongoose.connection;
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-require('./db.js');
 
 
-//var db = mongoose.connection;
+/* handle for questions collection in db. collections contain documents.
+ each document is an instance of the questionSchema. */
+var questions = require("./models/questionSchema.js");
 
-var question = require("./models/questionSchema.js");
 
+
+//==============================================================================
 app.get('/question', function(req,res){
-  //mongoose.model('users').find(function(err, users){
-    mongoose.model('usermodel').find(function(err, usermodel){
-      res.send(usermodel);
-    });
+
+  /*  Response should have this format:
+    {
+      question: "Who was the first computer programmer?",
+      answerId: 1
+    }
+  */
+  questions.findOne({'answerID': 1}, function (err, questionData){
+    if (err) return handleError(err);
+    console.log(questionData);
+
+    var questionText = questionData.question;
+    var answerID = questionData.answerID;
+    var questionResonseData = {
+      question: questionText,
+      answerId: answerID
+    }
+    res.json(questionResonseData);
+  });
 
 });
+//==============================================================================
+
+
+
 
 
 
@@ -34,9 +54,6 @@ app.get('/question', function(req,res){
 app.get('/questions', function(req, res) {
     res.send({ questions: questions });
 });
-
-
-
 
 app.post('/questions', function(req, res) {
     var questionType = req.body.name;
@@ -49,11 +66,6 @@ app.post('/questions', function(req, res) {
 
     res.send('Successfully created question!');
 });
-
-
-
-
-
 
 app.listen(PORT, function() {
     console.log('Server listening on ' + PORT);
