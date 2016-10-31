8a7d93d9e127d9bc2b727b76a5cbbf5904ed12b5
esotericabsurdist <esotericabsurdist@gmail.com>
2016-10-31 16:22:44 -0700

    built post funtion to insert new question

--- server.js
+++ server.js
@@ -9,17 +9,21 @@ var db = mongoose.connection;
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-
+app.listen(PORT, function() {
+    console.log('Server listening on ' + PORT);
+});
 
 /* handle for questions collection in db. collections contain documents.
  each document is an instance of the questionSchema. */
 var questions = require("./models/questionSchema.js");
+var questionCount = 2; // for testing, we have a 2 questions already.
+
+console.log(questionData);
 
 
 
 //==============================================================================
 app.get('/question', function(req,res){
-
   /*  Response should have this format:
     {
       question: "Who was the first computer programmer?",
@@ -28,10 +32,10 @@ app.get('/question', function(req,res){
   */
   questions.findOne({'answerID': 1}, function (err, questionData){
     if (err) return handleError(err);
-    console.log(questionData);
 
     var questionText = questionData.question;
     var answerID = questionData.answerID;
+
     var questionResonseData = {
       question: questionText,
       answerId: answerID
@@ -41,32 +45,34 @@ app.get('/question', function(req,res){
 
 });
 //==============================================================================
+app.post('/question', function(req, res) {
+    /* Should receive this format.
+    {
+      question: "Who led software development for NASA's Apollo lunar mission?",
+      answer: "Margaret Hamilton"
+    }
+    */
 
+    // Get data:
+    var questionText = req.body.question;
+    var answerText = req.body.answer;
+    var answerID = questionCount + 1;
 
+    var newQuestion = {
+      question: questionText,
+      answer: answerText,
+      answerId: answerID
+    }
 
-
-
-
-
-
-
-
-app.get('/questions', function(req, res) {
-    res.send({ questions: questions });
-});
-
-app.post('/questions', function(req, res) {
-    var questionType = req.body.name;
-    currentId++;
-
-    questions.push({
-        id: currentId,
-        name: questionType
+    questions.insert(newQuestion, function(err, data){
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log("Inserted New Question");
+      }
     });
 
-    res.send('Successfully created question!');
+    res.end();
 });
-
-app.listen(PORT, function() {
-    console.log('Server listening on ' + PORT);
-});
+//==============================================================================
